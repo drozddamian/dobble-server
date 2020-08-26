7c287def4b21a80758576701253d662054a97fb9
Damian Drozd <damiandrozd.dev@gmail.com>
2020-08-26 13:07:39 +0200

    * Quickfix, remove password field from mappedPlayerResponse

--- src/utils/apiResponseMapper.ts
+++ src/utils/apiResponseMapper.ts
@@ -3,17 +3,15 @@ import { IRoom } from '../models/Room'
 
 interface MappedPlayer {
   username: string;
-  password: string;
   nick: string;
   owningRooms?: IRoom[];
   joinedRooms?: IRoom[];
 }
 
 export const mapPlayerData = (player: IPlayer): MappedPlayer => {
-  const { username, password, nick, owningRooms, joinedRooms } = player
+  const { username, nick, owningRooms, joinedRooms } = player
   return {
     username,
-    password,
     nick,
     owningRooms,
     joinedRooms
